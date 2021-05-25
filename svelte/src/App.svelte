28261f6451bf7493a6b3c1025aa51206c7d5ed84
steven <github@stevenwaterman.uk>
2021-05-25 10:55:31 +0100

    Add router to svelte

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,30 +1,14 @@
 <script lang="ts">
-	export let name: string;
+  import { Router, Link, Route } from "svelte-navigator";
+  import Home from "./home/index.svelte";
+  import Hi from "./hi/index.svelte";
+  import NotFound from "./notfound/index.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<h1>Hello</h1>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Router>
+  <Route path="/" component={Home}/>
+  <Route path="hi" component={Hi}/>
+  <Route component={NotFound}/>
+</Router>
--- svelte/src/main.ts
+++ svelte/src/main.ts
@@ -2,9 +2,8 @@ import App from './App.svelte'
 
 const app = new App({
   target: document.body,
-  props: {
-    name: 'test'
-  }
+  props: {}
 })
 
 export default app
+ 
